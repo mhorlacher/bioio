65699c44ca1fe4292d34c5f798e9470e1180d1b2
Marc Horlacher <marc.horlacher@gmail.com>
2023-03-23 20:09:13 -0400

    added encoding options and updated requirements

--- bioio/bin/serialize.py
+++ bioio/bin/serialize.py
@@ -10,17 +10,18 @@ from bioio.tf.utils import dataset_to_tensor_features, features_to_json_file, se
 @click.command()
 @click.argument('biospec')
 @click.option('--gzip', is_flag=True, default=False)
-@click.option('--compression-level', type=int, default=None)
+@click.option('--gzip-compression-level', type=int, default=None)
+@click.option('--encoding', type=str, default=None)
 @click.option('-t', '--out-tfrecord', required=True, type=str)
 @click.option('-f', '--out-features', type=str, default=None)
-def main(biospec, gzip, compression_level, out_tfrecord, out_features):
+def main(biospec, gzip, gzip_compression_level, encoding, out_tfrecord, out_features):
     dataset = load_biospec(biospec)
     print(dataset.element_spec)
     
     # compress tfrecords to gzip, if flag '--gzip' is set
     tfrecord_options = tf.io.TFRecordOptions(
         compression_type = 'GZIP' if gzip else None,
-        compression_level = compression_level if gzip else None,
+        compression_level = gzip_compression_level if gzip else None,
     )
     
     # determine dataset cardinality 
@@ -28,7 +29,7 @@ def main(biospec, gzip, compression_level, out_tfrecord, out_features):
     cardinality = int(cardinality) if cardinality > 0 else None
 
     with tf.io.TFRecordWriter(out_tfrecord, tfrecord_options) as tf_writer, tqdm.tqdm(total=cardinality) as pbar:
-        features = dataset_to_tensor_features(dataset, encoding='zlib')
+        features = dataset_to_tensor_features(dataset, encoding=encoding)
 
         # write features spec to json file
         if out_features is None:
