1454edba34274c15d190d1fae1b46b6cb1b64295
Marc Horlacher <marc.horlacher@gmail.com>
2023-03-27 21:52:32 -0400

    implemented counting with native python (faster)

--- bioio/bin/tfutils/count.py
+++ bioio/bin/tfutils/count.py
@@ -1,15 +1,37 @@
 # %%
+import struct
+
 import click
-import tensorflow as tf
+
+# %%
+def count_records(tfrecord):
+    count = 0
+    with open(tfrecord, 'rb') as tfr:
+        while True:
+            try:
+                # byte length
+                byte_len = tfr.read(8)
+                if len(byte_len) == 0:
+                    break
+
+                # crc length
+                proto_len = struct.unpack('q', byte_len)[0]
+
+                # crc length + proto + crc
+                _ = tfr.read(4 + proto_len + 4)
+
+                count +=1
+            except Exception:
+                print('Not a valid TFRecord file.')
+                break
+        
+    return count
 
 # %%
 @click.command()
 @click.argument('tfrecord')
 def main(tfrecord):
-    count = 0
-    for _ in tf.data.TFRecordDataset(tfrecord):
-        count += 1
-    print(count)
+    print(count_records(tfrecord))
 
 # %%
 if __name__ == '__main__':
