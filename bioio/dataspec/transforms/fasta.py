db8d6bc3449144accdd84babd06c7886712a5a77
Marc Horlacher <marc.horlacher@gmail.com>
2023-03-28 18:28:21 -0400

    changed Fasta s.t. it may also return the sequence as tf.stirng

--- bioio/dataspec/transforms/fasta.py
+++ bioio/dataspec/transforms/fasta.py
@@ -24,11 +24,14 @@ class Fasta():
             raise ValueError(f'Unknown strand: {strand}')
         
         if self.to_onehot:
-            sequence = tf.cast(sequence2onehot(sequence), self.tensor_spec.dtype)
+            sequence = tf.cast(sequence2onehot(sequence), tf.int8)
+            sequence.set_shape((None, 4))
+        else:
+            sequence = tf.convert_to_tensor(sequence)
+            sequence.set_shape((None, ))
         return sequence
 
     def __call__(self, example):
         # return better_py_function_kwargs(Tout=tf.int8)(self.fetch)(kwargs)
         tensor = better_py_function_kwargs(Tout=self.tensor_spec)(self.fetch)(example)
-        tensor.set_shape(self.tensor_spec.shape)
         return tensor
