2db378b52c67a2627b9f56db74740f8968396a0f
Marc Horlacher <marc.horlacher@gmail.com>
2023-04-12 14:13:16 -0400

    added cwd to import paths

--- bioio/dataspec/loader.py
+++ bioio/dataspec/loader.py
@@ -1,11 +1,19 @@
 # %%
+import sys
+import types
 import importlib
+import pathlib
 
 import yaml
 import tensorflow as tf
 
 # %%
 from bioio.tf.utils import dataset_from_iterable
+
+# %%
+# To import modules from the current directory
+# TODO: Give user option to add arbitrary paths to sys.path
+sys.path.append('.')
 
 # %%
 def import_object_from_string(import_string):
@@ -20,7 +28,7 @@ class BiospecLoader(yaml.SafeLoader):
         super(BiospecLoader, self).__init__(*args, **kwargs)
         
         # add constructors to Loader and associate with tag
-        self.add_constructor('!Iterable', self.constructor_iterable)
+        self.add_constructor('!PyIterable', self.constructor_iterable)
         self.add_constructor('!Transform', self.constructor_transform)
     
     # def constructor_source(self, loader, node):
@@ -50,12 +58,57 @@ class BiospecLoader(yaml.SafeLoader):
         if 'args' not in fields:
             fields['args'] = {}
 
-        transform = import_object_from_string(fields['object'])
+        transform_object = import_object_from_string(fields['object'])
+        if isinstance(transform_object, types.FunctionType):
+            transform_name = transform_object.__name__
+            # if transform is a function, we need to inject arguments
+            transform = lambda x: transform_object(x, **fields['args'])
+            transform.__name__ = transform_name
+        else:
+            # if transform is a class, we need to instantiate it
+            transform = transform_object(**fields['args'])
 
+            if not hasattr(transform, '__name__'):
+                transform_name = type(transform).__name__
+            else:
+                transform_name = transform.__name__
+            transform.__name__ = transform_name
+
+            # assert that the transform is callable (it could be an abritrary object)
+            assert callable(transform), f'Transform {fields["object"]} is not callable'
+
+        # # optionally, cast the output of the transform to a specific tensorflow dtype
+        if 'dtype' in fields:
+            try:
+                dtype = tf.dtypes.as_dtype(fields['dtype'])
+            except TypeError:
+                raise TypeError(f"Invalid dtype: {fields['dtype']}")
+            # if dtype is specified, we need to cast the output of the transform
+            transform_ = transform # this is to avoid infinite recursion
+            transform = lambda x: tf.cast(transform_(x), dtype)
+            transform.__name__ = transform_.__name__
+
+        if 'input' not in fields:
+            # if input is not specified, we assume the transform is called within another 
+            # transform and return the transform itself
+            return transform
+
+        # if input is not a tf.data.Dataset, we assume it's a nested structure of 
+        # tf.data.Dataset and zip them together to create a zipped tf.data.Dataset
         if not isinstance(fields['input'], tf.data.Dataset):
             fields['input'] = tf.data.Dataset.zip(fields['input'], name='zip')
-        
-        dataset = fields['input'].map(transform(**fields['args']), name=transform.__name__)
+
+        # map the transform over the input dataset
+        dataset = fields['input'].map(transform, name=transform.__name__)
+
+        # optionally, apply an additional stack of zero or more transforms to the dataset
+        if 'map' in fields:
+            for transform in fields['map']:
+                if isinstance(transform, tf.data.Dataset):
+                    raise TypeError('Expected a transform, but got a tf.data.Dataset. Did specify \'input\' in a nested context?')
+                dataset = dataset.map(transform, name=transform.__name__)
+
+        # TODO: Replace with 
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
 
         return dataset
